bf9d3e735504a1d49721557358cdb319775ca858
git config --global user.name <yonashaile12@gmail.com>
2020-11-28 12:34:57 -0600

    Configuration file and properties added

--- src/test/java/day4/DB_Test.java
+++ src/test/java/day4/DB_Test.java
@@ -1,32 +1,37 @@
 package day4;
 
-import org.junit.*;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Test;
 import utility.DB_Utility;
 
 public class DB_Test {
 
-    //create connection once and destroy it once
+    // create connection once and destroy it once
 
     @BeforeAll
     public static void init(){
         DB_Utility.createConnection();
     }
 
-
     @Test
     public void testEmployeeCount(){
-        //run a query SELECT * FROM EMPLOYEES
-        //assert that the employee count is 107
-        DB_Utility.runQuery("SELECT *  FROM EMPLOYEES");
-        DB_Utility.displayAllData();
-        Assertions.assertEquals(107, DB_Utility.getRowCount());
+
+        // run a query SELECT * FROM EMPLOYEES
+        // assert that the employees count is 107
+        DB_Utility.runQuery("SELECT * FROM EMPLOYEES") ;
+        //Assertions.assertEquals(107,  DB_Utility.getRowCount()  );
+
+
     }
+    // Open another test
+    // run query : SELECT * FROM REGIONS
+    // assert -- 3rd row second column is Asia
+    @Test
+    public void test3rdRowSecondColumn(){
 
+        DB_Utility.runQuery("SELECT * FROM REGIONS") ;
+       // Assertions.assertEquals("Asia" , DB_Utility.getColumnDataAtRow(3,2) );
 
-
-
+    }
 
 
     @AfterAll
--- src/test/java/day4/Junit5_Practice.java
+++ src/test/java/day4/Junit5_Practice.java
@@ -1,52 +1,51 @@
 package day4;
-
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.*;
 
-
+import org.junit.jupiter.api.Test;
 
 public class Junit5_Practice {
 
     // what can we use if we want to run certain code before all test only once
-    //@BeforeAll
+    // @BeforeAll
     @BeforeAll
     public static void setUp(){
         System.out.println("@BeforeAll IS RUNNING");
     }
-    // what can we use if we want to run certain code before test
-    //@BeforeEach
 
-    @BeforeAll
+    // what can we use if we want to run certain code before each test
+    // @BeforeEach
+    @BeforeEach
     public void beforeEachMethod(){
         System.out.println("@BeforeEach IS RUNNING");
     }
-    //what can we use if we want to run certain code after all test only once
-   // @AfterAll
+    // what can we use if we want to run certain code after all test only once
+    // @AfterAll
     @AfterAll
     public static void tearDown(){
         System.out.println("@AfterAll IS RUNNING");
     }
-    //what can we use if we want to run certain code after each test
-    //@AfterEach
+
+    // what can we use if we want to run certain code after each test
+    // @AfterEach
     @AfterEach
     public void AfterEachMethod(){
         System.out.println("@AfterEach IS RUNNING");
     }
 
+
     @Test
     public void test1(){
-        System.out.println("Test is running....");
-        Assertions.assertEquals(9,3+6);
+
+        System.out.println("Test1 is running");
+       // Assertions.assertEquals(9, 3+6);
 
     }
 
     @Test
     public void test2(){
-        System.out.println("Test 2 is running...");
+
+        System.out.println("Test2 is running");
+        //Assertions.assertEquals(9, 3+6);
+
     }
 }
